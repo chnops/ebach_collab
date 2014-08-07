840e9e5249737c59f62d523f20fba401d276d403
ebach <ebach@iastate.edu>
2014-08-07 11:52:05 -0500

    ordered SoilFrac in ascending order on x-axis

--- COBS_ITS_PhylaTotalsStacked.R
+++ COBS_ITS_PhylaTotalsStacked.R
@@ -2,6 +2,7 @@
 #COBS ITS Stacked Bar graphs of phyla
 #July 26, 2014
 
+#Use data_taxa2 from "diversity_stats.R" or "COBS_ITS_TargetTaxaNMDS.R"
 #Phyla-level diversity, summarizes total counts for each phylum
 Phyla.data2<-ddply(data_taxa2, .(SampleName, Date, Crop, Block, SoilFrac, Phylum), summarise, .drop=FALSE, .progress="text",total=sum(value))
 head(Phyla.data2)
@@ -25,4 +26,8 @@ ggplot(Phyla.CropT, aes(Crop, total, fill=Phylum))+geom_bar(stat="identity")
 #Total"values"
 Phyla.SoilFracT<-ddply(Phyla.data2, .(SoilFrac,Phylum), summarise,.progress="text",total=sum(total))
 head(Phyla.SoilFracT)
+print(levels(Phyla.SoilFracT$SoilFrac))
+Phyla.SoilFracT$SoilFrac=factor(Phyla.SoilFracT$SoilFrac, levels(Phyla.SoilFracT$SoilFrac)[c(2,4,3,1,5)])
+print(levels(Phyla.SoilFracT$SoilFrac))
+
 ggplot(Phyla.SoilFracT, aes(SoilFrac, total, fill=Phylum))+geom_bar(stat="identity")
